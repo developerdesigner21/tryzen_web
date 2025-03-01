32cb29585cb1c50ff499bb66e9c8f32c5a6708c0
harsh <harsh@gmail.com>
2025-03-01 17:39:24 +0530

    3rd attempt

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function ScrollToTop() {
 function LoadingScreen() {
   return (
     <div className="loading-screen">
-      <div className="img-dots-container w-full max-w-[75vw] sm:max-w-[50vw] lg:max-w-[25vw] h-auto mb-16">
+      <div className="img-dots-container w-full max-w-[75%] sm:max-w-[50%] lg:max-w-[25%] h-auto mb-16">
         <LoadingIcon />
         <div className="loading-dots">
           <span className="dot"></span>
